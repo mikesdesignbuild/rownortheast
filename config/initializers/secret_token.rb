25650947b3b2a9a545286a9befac4e37b238f5b2
Mike Plusch <mikeplusch@gmail.com>
2014-01-25 16:43:48 -0500

    rails new rownortheast

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rownortheast::Application.config.secret_key_base = '5bffdba4e8523e1f87200f065e59500fe56571057a2d1d9349e42e3f11a2cba1f11b4076491ae9747aaf1861a0daa4f23ffdb91d7cfe16bb63cea9b7e5c9c1d4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rownortheast::Application.config.session_store :cookie_store, key: '_rownortheast_session'
